3f1ba71f1364641e4fa7f399e0f050bf657c8f1b
Vinicius Santos <viniciuspereira2244@gmail.com>
2022-02-11 19:38:57 -0300

    Adicionando o typescript e incrementando

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response, NextFunction} from "express";
+
+const app = express();
+
+app.use('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'Sucesso Total!'});
+});
+
+app.listen(3000, () =>{
+    console.log('Aplicação executando porta 3000!');
+});
